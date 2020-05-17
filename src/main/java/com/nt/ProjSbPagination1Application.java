ba255feb07feeb855ee2a640f1b2add42fb22db4
renarena-3 <52742284+renarena-3@users.noreply.github.com>
2020-05-17 09:12:21 +0530

    Update ProjSbPagination1Application.java
    yes changes has done

--- src/main/java/com/nt/ProjSbPagination1Application.java
+++ src/main/java/com/nt/ProjSbPagination1Application.java
@@ -8,7 +8,8 @@ public class ProjSbPagination1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProjSbPagination1Application.class, args);
-		System.out.println("done");
+		System.out.println("1 done");
+		System.out.println("2 done");
 	}
 
 }
